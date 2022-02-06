d4c47304bae916fad266db463a12ddc9b4f10d6b
x <x@qq.com>
2022-02-06 21:43:27 +0800

    feat: read image with channel 1

--- tests/test_ncnn_clazz.cpp
+++ tests/test_ncnn_clazz.cpp
@@ -48,7 +48,8 @@ void saveImgRawData(const char *filename, unsigned char *raw, int w, int h, int 
 int readImgData(const char *filename, XNNImage &image) {
     // read image
     int h, w, channel;
-    int desired_channels = 0;
+    // read image with channel 1
+    int desired_channels = 1;
     #ifdef USE_STB
     auto input_image = stbi_load(filename, &w, &h, &channel, desired_channels);
     if (!input_image)
